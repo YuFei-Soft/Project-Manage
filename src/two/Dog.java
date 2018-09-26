eac501e157e74af0882447e8691a5810adb02784
PengFeiDing <214681827@qq.com>
2018-09-26 21:36:33 +0800

    新加代码

--- src/two/Dog.java
+++ src/two/Dog.java
@@ -1,18 +1,47 @@
 package two;
 
 public class Dog extends Pet {
-	
+	private String strain;
+
+	public String getStrain() {
+		return strain;
+	}
+
+	public void setStrain(String strain) {
+		this.strain = strain;
+	}
+
+	public Dog() {
+
+	}
+
+	public Dog(String name, int health, int love, String strain) {
+		super(name, health, love);
+		this.strain = strain;
+	}
+
+	@Override
+	public String toString() {
+		return "Dog [strain=" + strain + "]" + ",getNmame()" + getName()
+				+ ",getHealth()=" + getHealth() + ",getLove()" + getLove()
+				+ "]";
+	}
 
 	@Override
 	public void print() {
-		// TODO Auto-generated method stub
-
+		System.out.println("Dog [strain=" + strain + "]" + ",getNmame()"
+				+ getName() + ",getHealth()=" + getHealth() + ",getLove()"
+				+ getLove() + "]");
 	}
 
 	@Override
 	public void eat() {
-		// TODO Auto-generated method stub
-
+		if (getHealth() < 70) {
+			setHealth(getHealth() + 10);
+			System.out.println("狗吃食，体力增加：" + getHealth());
+		} else {
+			System.out.println("狗不饿，不需要吃食");
+		}
 	}
 
 }
---
+++ src/two/Pig.java
@@ -0,0 +1,20 @@
+package two;
+
+public class Pig extends Pet {
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void eat() {
+		if (getHealth() < 80) {
+			setHealth(getHealth() + 20);
+		} else {
+			System.out.println("猪不饿，不需要吃食");
+		}
+	}
+
+}
