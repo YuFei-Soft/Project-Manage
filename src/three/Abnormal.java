1fd55b79210b61d7c1e2a84422eb83f55d7fb9ce
PengFeiDing <214681827@qq.com>
2018-09-29 21:49:21 +0800

    添加log4j

--- src/three/Abnormal.java
+++ src/three/Abnormal.java
@@ -1,9 +1,18 @@
 package three;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import org.apache.log4j.Logger;
+
 public class Abnormal {
+	 static Logger logger = Logger.getLogger(Abnormal.class);
+	//static Logger logger=Logger.getLogger(Abnormal.class);
 	public static void main(String[] args) {
+		logger.debug("main函数开始执行");
+		logger.info("main函数开始执行");
+		logger.error("main函数开始执行");
+		logger.warn("main函数开始执行");
 		Scanner input = new Scanner(System.in);
 		System.out.println("请输入课程代号（1—3之间的数字）");
 		try {
@@ -20,6 +29,7 @@ public class Abnormal {
 				break;
 			}
 		} catch (Exception e) {
+			logger.debug("出现异常：{}",e);
 			// e.printStackTrace();
 			System.out.println("出现异常！退出程序！");
 		} finally {
