147125f2ed29bb95f446219dcbb232673bb50f24
PengFeiDing <214681827@qq.com>
2018-10-08 15:24:04 +0800

    游戏练习

---
+++ src/youxi/Level.java
@@ -0,0 +1,64 @@
+package youxi;
+
+public class Level {
+	private int levelNo;// 各级别的号
+	private int strLength;// 各级别一次输出字符串的长度
+	private int strTimes;// 各级别输出字符串的次数
+	private int timeLimit;// 各级别闯关的时间限制
+	private int perScore;// 各级别正确输入一次得分
+
+	public int getLevelNo() {
+		return levelNo;
+	}
+
+	public void setLevelNo(int levelNo) {
+		this.levelNo = levelNo;
+	}
+
+	public int getStrLength() {
+		return strLength;
+	}
+
+	public void setStrLength(int strLength) {
+		this.strLength = strLength;
+	}
+
+	public int getStrTimes() {
+		return strTimes;
+	}
+
+	public void setStrTimes(int strTimes) {
+		this.strTimes = strTimes;
+	}
+
+	public int getTimeLimit() {
+		return timeLimit;
+	}
+
+	public void setTimeLimit(int timeLimit) {
+		this.timeLimit = timeLimit;
+	}
+
+	public int getPerScore() {
+		return perScore;
+	}
+
+	public void setPerScore(int perScore) {
+		this.perScore = perScore;
+	}
+
+	public Level(int levelNo, int strLength, int strTimes, int timeLimit,
+			int perScore) {
+		super();
+		this.levelNo = levelNo;
+		this.strLength = strLength;
+		this.strTimes = strTimes;
+		this.timeLimit = timeLimit;
+		this.perScore = perScore;
+	}
+
+	public Level() {
+		super();
+	}
+
+}
