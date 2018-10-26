d4d741de3fe949f42b676fd5b24108f90ad1014b
PengFeiDing <214681827@qq.com>
2018-10-26 23:01:38 +0800

    修改

--- src/kaoshi2/Test2.java
+++ src/kaoshi2/Test2.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Test2 {
-public static void main(String[] args) {
-	Scanner input=new Scanner(System.in);
-	int [] a=new int[10];
-	for(int i=0;i<10;i++){
-	System.out.println("请输入第"+(i+1)+"学生的成绩：");
-		a[i]=input.nextInt();	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int[] a = new int[10];
+		for (int i = 0; i < 10; i++) {
+			System.out.println("请输入第" + (i + 1) + "学生的成绩：");
+			a[i] = input.nextInt();
+		}
+		Arrays.sort(a);
+		int b = a[0];
+		System.out.println("最小分数为；" + b);
 	}
-	Arrays.sort(a);
-	int b=a[0];
-	System.out.println("最小分数为；"+b);
-}
 }
--- src/kaoshi2/Test6.java
+++ src/kaoshi2/Test6.java
@@ -2,25 +2,20 @@ package kaoshi2;
 
 import java.util.Random;
 
-
 public class Test6 {
-	public static void main(String[] args) {	
-		Random rand=new Random();
-		int number=rand.nextInt(100)+1;//得到100的随机数，不从1开始
-		while(true){
-			if(number%2==0){
-				number=number/2;				
-			}else{
-				number=number*3+1;
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int number = rand.nextInt(100) + 1;// 得到100的随机数，不从1开始
+		while (true) {
+			if (number % 2 == 0) {
+				number = number / 2;
+			} else {
+				number = number * 3 + 1;
 			}
-			if(number==1){
+			if (number == 1) {
 				System.out.println(number);
-				break;			
+				break;
 			}
-			
 		}
-		
-	
-
 	}
 }
